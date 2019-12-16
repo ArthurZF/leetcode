9dd434d0ecfc85375cd5c8df28c6dfc5c0c081d6
LHCX <13259854047@163.com>
2019-12-16 13:33:23 +0800

    35 finish

---
+++ 35.搜索插入位置.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (44.66%)
+ * Likes:    377
+ * Dislikes: 0
+ * Total Accepted:    98.1K
+ * Total Submissions: 219.6K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  for (let index = 0; index < nums.length; index++) {
+    if (index == 0 && nums[index] >= target) {
+      return index;
+    } else if (target == nums[index]) {
+      return index;
+    } else if (nums[index] < target && target < nums[index + 1]) {
+      return index + 1;
+    } else if (index == nums.length - 1 && nums[index] < target) {
+      return nums.length;
+    }
+  }
+};
+// @lc code=end
